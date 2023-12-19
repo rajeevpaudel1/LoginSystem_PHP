3c54a9d128fb6dd3c2869e969e29be21f497aaae
Rajeev Paudel <rajeev11paudel@gmail.com>
2023-12-19 16:08:01 +0545

    File added successfully

--- partials/nav.php
+++ partials/nav.php
@@ -1,6 +1,12 @@
 <!--navigation bar starts!-->
-
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<?php 
+if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
+  $loggedin= true;
+}
+else{
+  $loggedin=false;
+}
+echo'<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <a class="navbar-brand" href="/LoginSystem_PHP">isecure</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
@@ -10,23 +16,27 @@
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
         <a class="nav-link" href="/LoginSystem_PHP/welcome.php">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
+      </li>';
+        if(!$loggedin){
+      echo '<li class="nav-item">
         <a class="nav-link" href="/LoginSystem_PHP/login.php">Login</a>
       </li>
 
       <li class="nav-item">
         <a class="nav-link" href="/LoginSystem_PHP/signup.php">Signup</a>
-      </li>
-      <li class="nav-item">
+      </li>';
+        }
+      if($loggedin){
+      echo '<li class="nav-item">
         <a class="nav-link" href="/LoginSystem_PHP/logout.php">logout</a>
-      </li>
-
-    </ul>
+      </li>';
+}
+   echo ' </ul>
     <form class="form-inline my-2 my-lg-0">
       <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
       <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
     </form>
   </div>
-</nav>
+</nav>'
+?>
 <!--navigation bar ends!-->
